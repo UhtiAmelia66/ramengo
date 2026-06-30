ec04e6391e24d9dea77f6e7c3a1c17bd98ae97bb
UhtiAmelia66 <uhtiamelia06@gmail.com>
2026-06-30 23:14:18 +0700

    fix vercel laravel runtime 500

--- api/index.php
+++ api/index.php
@@ -1,3 +1,46 @@
 <?php
 
+$runtimeDirectories = [
+    '/tmp/storage',
+    '/tmp/storage/app',
+    '/tmp/storage/framework',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($runtimeDirectories as $directory) {
+    if (! is_dir($directory)) {
+        mkdir($directory, 0777, true);
+    }
+}
+
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+$_SERVER['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+$_SERVER['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
+$_SERVER['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
+
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes-v7.php');
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes-v7.php';
+$_SERVER['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes-v7.php';
+
+putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/bootstrap/cache/events.php';
+$_SERVER['APP_EVENTS_CACHE'] = '/tmp/bootstrap/cache/events.php';
+
 require __DIR__.'/../public/index.php';
